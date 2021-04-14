db8f6116b5c9ef14de1e2a037d01d6042836012d
NazarSqa <56995245+NazarSqa@users.noreply.github.com>
2021-04-14 19:56:02 +0300

    Add files via upload

---
+++ PP4/wetherAPIser/wetherAPIser/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace wetherAPIser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.City = new System.Windows.Forms.TextBox();
+            this.wInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(118, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // City
+            // 
+            this.City.Location = new System.Drawing.Point(12, 12);
+            this.City.Name = "City";
+            this.City.Size = new System.Drawing.Size(100, 20);
+            this.City.TabIndex = 1;
+            // 
+            // wInfo
+            // 
+            this.wInfo.BackColor = System.Drawing.Color.Transparent;
+            this.wInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.wInfo.ForeColor = System.Drawing.Color.White;
+            this.wInfo.Location = new System.Drawing.Point(12, 45);
+            this.wInfo.Name = "wInfo";
+            this.wInfo.Size = new System.Drawing.Size(168, 84);
+            this.wInfo.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(234, 211);
+            this.Controls.Add(this.wInfo);
+            this.Controls.Add(this.City);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox City;
+        private System.Windows.Forms.Label wInfo;
+    }
+}
+
---
+++ PP4/wetherAPIser/wetherAPIser/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Drawing;
+
+namespace wetherAPIser
+{
+    public partial class Form1 : Form
+    {
+        public Form1() 
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*1*/
+            string URL_Sity = "http://" + "api.openweathermap.org/data/2.5/weather?q=" + City.Text + "&&units=metric&&appid=74415db07be3b557da5ee20fbbd395ae";
+            string url = URL_Sity;
+
+
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+
+            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+            string response;
+
+            using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+            {
+                response = streamReader.ReadToEnd();
+            }
+            
+            WatherRespon watherResp = JsonConvert.DeserializeObject<WatherRespon>(response);
+
+
+            if (watherResp.Main.Temp >= 20)
+            {
+                this.BackgroundImage = Properties.Resources.Son;
+                this.Size = new Size(602,604);
+                wInfo.ForeColor = Color.Navy;
+
+            }
+            else { 
+                this.BackgroundImage = Properties.Resources.Rain;
+                this.Size = new Size(560,315);
+                wInfo.ForeColor = Color.White;
+            }
+
+
+            /*2*/wInfo.Text = "Temperature in" +" "+watherResp.Main.Temp + "°C" + "\n"+ watherResp.Name;
+        }
+    }
+}
